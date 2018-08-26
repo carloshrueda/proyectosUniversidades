eff907d18fca1d1f9b1785f0ada0c5b600905d56
Carlos Rueda <carloshrueda@gmail.com>
2018-08-26 00:29:32 -0500

    NumeroMistico
    Se terminó la implementación del ejercicio

--- Unab/src/ejercicios_20180825/NumeroMistico.java
+++ Unab/src/ejercicios_20180825/NumeroMistico.java
@@ -26,26 +26,48 @@ import java.util.Scanner;
 
 public class NumeroMistico {
 
+	public int numeroMistico(String nombre) {
+		// letras: A  B  C  D  E  F  G  H  I  J  K  L  M  N  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
+		//         1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25  26 27
+		int nm = 0;
+		char letra;
+		int valletra;
+
+		for (int i = 0; i <= nombre.length() - 1; i++) {
+			letra = nombre.charAt(i);
+			if ((letra >= 'A') && (letra <= 'Z')) {
+				valletra = ((int) letra - 64); // valor de la letra en entero
+				if (letra > 'N') 
+					valletra = valletra + 1;
+				System.out.println(letra + " <-> " + valletra);
+				nm = nm + valletra; // igual: nm + ((int) nombre.charAt(i) - 65 + 1);
+			}
+
+		}
+
+		return nm;
+	}
+
 	public static void main(String[] args) {
 		String opcion = "";
+		String nombre;
 
 		Scanner scn = new Scanner(System.in);
-		Conversor convertir = new Conversor();
+		NumeroMistico nm = new NumeroMistico();
 
 		do {
-			System.out.print("\nIngrese un número entero positivo a convertir a binario?: ");
-			nument = scn.nextInt();
+			System.out.print("\nNombre?: ");
+			nombre = scn.nextLine().trim();
 
-			if (nument >= 0) {
-				numbin = convertir.dec2bin(nument);
-				System.out.println("El número en binario es: " + numbin);
+			if (nombre.isEmpty() == false) {
+				nombre = nombre.toUpperCase();
+				System.out.println("El número místido es: " + nm.numeroMistico(nombre));
 			} else {
-				System.out.println("Error. número invalido. Ingrese un número enteror positivo.");
-
+				System.out.println("Error. El nombre está vacío.");
 			}
 
 			System.out.print("\nDesea continuar (S/N)?: ");
-			opcion = scn.next().toUpperCase();
+			opcion = scn.nextLine().toUpperCase();
 		} while (opcion.equals("S") == true);
 
 		scn.close();
