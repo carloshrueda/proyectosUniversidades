834d8acbabac6fd216c1a98aad90792d2f8461aa
Carlos Rueda <carloshrueda@gmail.com>
2018-08-25 23:33:57 -0500

    Fibonacci
    Se crea la clase

---
+++ Unab/src/ejercicios_20180825/Fibonacci.java
@@ -0,0 +1,20 @@
+package ejercicios_20180825;
+
+/*
+ * Realizar un programa que muestre la serie de Fibonacci
+ * desde le primer numero "n1" que digite el usuario, hasta el 
+ * número que sea menor o igualal numero "n2" que digite el
+ * usuario.
+ * 
+ * El programa debe repetirse hasta que el usuario lo desee 
+ *  
+ */
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
