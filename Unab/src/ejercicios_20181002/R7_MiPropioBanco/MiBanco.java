e09401b4a24067bebe2ff43871181fd9e14f5227
Carlos Rueda <carloshrueda@gmail.com>
2018-10-02 20:19:04 -0500

    Clase MiBanco
    Se creo la clase MiBanco

---
+++ Unab/src/ejercicios_20181002/R7_MiPropioBanco/MiBanco.java
@@ -0,0 +1,29 @@
+package ejercicios_20181002.R7_MiPropioBanco;
+
+public class MiBanco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+/* R7: MI PROPIO BANCO
+
+Realiza un programa que me permita simular un banco manejando clientes y sus cuentas,
+mediante un menú así:
+1. Ver lista de clientes: Lista de los nombre de lso clientes y número de
+   cuenta, los números de cuenta los controla el sistema y son consecutivos
+2. Ver saldo de un cliente a partir de su numero de cuenta
+3. Crear un cliente con nombre y saldo inicial
+4. Editar el nombre de un cliente. a partir de un listado de clientes
+5. Depositar dinero a una cuenta (antes de confirmar el depósito mostrar 
+   de que cliente es la cuenta))
+6. Retirar dinero de una cuenta (no puede retirar más dinero del que hay)
+7 Cancelar una cuenta: Eliminar una cuenta y al cliente. Debe emitir un 
+  cheque por el saldo de la cuenta antes de cancelarla (mostrar un
+  mensaje en pantalla)
+8. Salir del banco.
+*/
