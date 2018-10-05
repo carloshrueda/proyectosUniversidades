bfdc6af1e9f7630b50a8bf2dad663051334e035f
Carlos Rueda <carloshrueda@gmail.com>
2018-10-05 07:22:12 -0500

    avance

--- Unab/src/ejercicios_20181002/RA7_Ideota/Carro.java
+++ Unab/src/ejercicios_20181002/RA7_Ideota/Carro.java
@@ -1,27 +1,59 @@
 package ejercicios_20181002.RA7_Ideota;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Carro {
-    int[] CantidadTipoFrutas = new int[TipoFruta.values().length];
-    double pesoTotal;
-    double caloriasTotal;
-    long precioTotal;
-    
-     public Carro() {
+	private ArrayList<Fruta> lstFrutas;
+	private double pesoTotal;
+	private double caloriasTotal;
+	private long precioTotal;
+
+	public Carro() {
         this.pesoTotal = 0.0;
         this.caloriasTotal = 0.0;
         this.precioTotal = 0;
-        Arrays.fill(CantidadTipoFrutas, 0);
+        lstFrutas = new ArrayList<Fruta>();
     }
-     
-    public void addFruta(Fruta fruta) {
-        if (fruta != null) {
-            this.pesoTotal += fruta.getPeso();
-            this.caloriasTotal += fruta.getCalorias();
-            this.precioTotal += fruta.getPrecio();
-            ++this.CantidadTipoFrutas[fruta.getTipo().ordinal()];
-        }
-    }
-    
+
+	public void addFruta(Fruta fruta) {
+		if (fruta != null) {
+			this.pesoTotal += fruta.getPeso();
+			this.caloriasTotal += fruta.getCalorias();
+			this.precioTotal += fruta.getPrecio();
+			lstFrutas.add(fruta);
+		}
+	}
+
+	public ArrayList<Fruta> getLstFrutas() {
+		return lstFrutas;
+	}
+
+	public double getPesoTotal() {
+		return pesoTotal;
+	}
+
+	public double getCaloriasTotal() {
+		return caloriasTotal;
+	}
+
+	public long getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setLstFrutas(ArrayList<Fruta> lstFrutas) {
+		this.lstFrutas = lstFrutas;
+	}
+
+	public void setPesoTotal(double pesoTotal) {
+		this.pesoTotal = pesoTotal;
+	}
+
+	public void setCaloriasTotal(double caloriasTotal) {
+		this.caloriasTotal = caloriasTotal;
+	}
+
+	public void setPrecioTotal(long precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+
 }
--- Unab/src/ejercicios_20181002/RA7_Ideota/TestFruta.java
+++ Unab/src/ejercicios_20181002/RA7_Ideota/TestFruta.java
@@ -6,13 +6,122 @@ package ejercicios_20181002.RA7_Ideota;
  * @author ESTUDIANTE
  */
 public class TestFruta {
-    
-    public static void main(String[] args) {
-        
-        for(TipoFruta t:TipoFruta.values()){
-            System.out.println("tipo: "+t.name());
-            
-        }
-    }
-    
+
+	public static void agregarFrutaCarrito(Carro carrito) {
+		InterfazUtilidades.tituloMenu("1. Agregar Fruta al Carrito de Compras");
+
+		System.out.println("Frutas disponibles:");
+		int cont = 0;
+		for (TipoFruta fruta : TipoFruta.values()) {
+			System.out.println(++cont + ". " + fruta.name());
+		}
+
+		//int maxfruta = TipoFruta.values().length;
+		System.out.println("\nQue fruta de las disponibles desea agregar (1, " + cont + ")? ");
+		int opcion = Entrada.entero();
+		if ((opcion < 1) || (opcion > cont)) {
+			System.out.println(">>> Error. La opción de fruta no esta disponible.");
+			System.out.println(
+					"\n>>> Fin Agregar Fruta al Carrito de Compras. \nPresione cualquier tecla para continuar....");
+			Entrada.cadena();
+			return;
+		}
+
+		switch (opcion - 1) {
+		case 0:
+			carrito.addFruta(new Papaya());
+			break;
+			
+		case 1:
+			carrito.addFruta(new Kiwi());
+			break;
+			
+		case 2:
+			carrito.addFruta(new Mango());
+			break;
+			
+		case 3:
+			carrito.addFruta(new Banano());
+			break;
+			
+		case 4:
+			carrito.addFruta(new Naranja());
+			break;
+
+		default:
+			break;
+		}
+
+		System.out.println(
+				"\n>>> Fin Agregar Fruta al Carrito de Compras. \nPresione cualquier tecla para continuar....");
+		Entrada.cadena();
+	}
+	
+	public static void listarFrutasCarrito(Carro carrito) {
+		InterfazUtilidades.tituloMenu("2. Listar Frutas del Carrito de Compras");
+
+		System.out.println("Las frutas en el carrito de compras son:");
+		for (Fruta fruta : carrito.getLstFrutas()) {
+			System.out.println(fruta.getNombre());
+		}
+
+		System.out.println(
+				"\n>>> Fin Listado del Carrito de Compras. \nPresione cualquier tecla para continuar....");
+		Entrada.cadena();
+	}
+
+	public static void main(String[] args) {
+		int opcion;
+		Carro carrito = new Carro();
+
+		do {
+			opcion = InterfazUtilidades.Menu("SUPERMERCA FRUTAS",
+					new String[] { " 1. Agregar Fruta al Carrito de Compras",
+							" 2. Listar Frutas del Carrito de Compras",
+							" 3. Listar Total Calorias del Carrito de Compras",
+							" 4. Calcular Precio del Carrito de Compras", " 5. Salir" },
+					1, 5);
+
+			switch (opcion) {
+
+			case 1:
+				agregarFrutaCarrito(carrito);
+				break;
+				
+			case 2:
+				listarFrutasCarrito(carrito);
+				break;
+
+			/*
+			 * case 2: listarCargos(empresa); break;
+			 * 
+			 * case 3: editarNombreCargo(empresa); break;
+			 * 
+			 * case 4: editarSueldoCargo(empresa); break;
+			 * 
+			 * case 5: reorganizarJerarquia(empresa); break;
+			 * 
+			 * case 6: agregarEmpleado(empresa); break;
+			 * 
+			 * case 7: listarEmpleadoCargo(empresa); break;
+			 * 
+			 * case 8: editarNombreEmpleado(empresa); break;
+			 * 
+			 * case 9: buscarEmpleadoCargoParteNombre(empresa); break;
+			 * 
+			 * case 10: listarEmpleadoCargoNombre(empresa); break;
+			 * 
+			 * case 11: calcularNomina(empresa); break;
+			 */
+			default:
+				break;
+			}
+
+		} while (opcion != 5);
+
+		// teclado.close();
+
+		System.out.println("\n>>> GRACIAS POR USAR EL SOFTWARE. ADIOS.");
+	}
+
 }
